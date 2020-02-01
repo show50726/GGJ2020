367a354c2715c9c2301d297449999ba66d8672db
bongbonggun <bongbonggun@gmail.com>
2020-02-01 16:22:57 +0800

    1.player jump optimaztion 2.auto assign key 3.add player2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool isClimb = false;
-    public Rigidbody2D player1;
-    public Rigidbody2D player2;
+    public bool isClimbing = false;
+    public bool isJumping = false;
+    private Rigidbody2D player;
 
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+
+    public KeyCode keyLeft;
+    public KeyCode keyRight;
+    public KeyCode keyClimb;
+    public KeyCode keyJump;
+    public KeyCode keyAttack;
 
     enum MoveParameter
     {
@@ -17,71 +23,98 @@ public class PlayerController : MonoBehaviour
         CLIMB,
         JUMP
     }
-    enum Player1Move
-    {
-        LEFT = KeyCode.A,
-        RIGHT = KeyCode.D,
-        CLIMB = KeyCode.W,
-        JUMP = KeyCode.Space,
-        ATTACK = KeyCode.LeftControl
-    }
-    enum Player2Move
-    {
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (isClimb && Input.GetKey((KeyCode)Player1Move.CLIMB))
-        {
-            player1.AddForce(Vector2.up * GetMoveParameter(MoveParameter.CLIMB), ForceMode2D.Force);
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                player1.AddForce(Vector2.left * GetMoveParameter(MoveParameter.RUN), ForceMode2D.Force);
-            }
-            else if (Input.GetKey((KeyCode)Player1Move.RIGHT))
-            {
-                player1.AddForce(Vector2.right * GetMoveParameter(MoveParameter.RUN), ForceMode2D.Force);
-            }
-
-            if (Input.GetKey((KeyCode)Player1Move.JUMP))
-            {
-                if (player1.velocity.y < 0)
-                {
-                    player1.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-                }
-                //else if (player1.velocity.y > 0 && !Input.GetButton("Jump"))
-                else if (player1.velocity.y > 0)
-                {
-                    player1.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-                }
-            }
-        }
-    }
-
     float GetMoveParameter(MoveParameter move)
     {
         switch (move)
         {
             case MoveParameter.RUN:
-                return 10;
+                return 30;
             case MoveParameter.CLIMB:
-                return 10;
+                return 30;
             case MoveParameter.JUMP:
-                return 50;
+                return 30;
         }
 
         // unhandled
         return 0;
+    }
+    void AssignKey()
+    {
+        switch (player.name)
+        {
+            case "Player1":
+                keyLeft = KeyCode.S;
+                keyRight = KeyCode.D;
+                keyClimb = KeyCode.UpArrow;
+                keyJump = KeyCode.Space;
+                keyAttack = KeyCode.LeftControl;
+                break;
+            case "Player2":
+                keyLeft = KeyCode.Keypad4;
+                keyRight = KeyCode.Keypad6;
+                keyClimb = KeyCode.Keypad8;
+                keyJump = KeyCode.Keypad0;
+                keyAttack = KeyCode.KeypadEnter;
+                break;
+            default:
+                keyLeft = KeyCode.S;
+                keyRight = KeyCode.D;
+                keyClimb = KeyCode.UpArrow;
+                keyJump = KeyCode.Space;
+                keyAttack = KeyCode.LeftControl;
+                break;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+
+        AssignKey();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isClimbing && Input.GetKey(keyClimb))
+        {
+            player.AddForce(Vector2.up * GetMoveParameter(MoveParameter.CLIMB), ForceMode2D.Force);
+        }
+        else
+        {
+            if (Input.GetKey(keyLeft))
+            {
+                player.AddForce(Vector2.left * GetMoveParameter(MoveParameter.RUN), ForceMode2D.Force);
+            }
+            else if (Input.GetKey(keyRight))
+            {
+                player.AddForce(Vector2.right * GetMoveParameter(MoveParameter.RUN), ForceMode2D.Force);
+            }
+
+            if (Input.GetKeyDown(keyJump) && !isJumping)
+            {
+                player.velocity = Vector2.up * 8f;
+            }
+
+            if (Input.GetKeyUp(keyJump))
+            {
+                isJumping = true;
+            }
+
+            RaycastHit2D hit = Physics2D.Raycast(player.transform.position, -Vector2.up, 1f, ~(1 << LayerMask.NameToLayer("Player")));
+            if (hit.collider != null && isJumping)
+            {
+                isJumping = false;
+            }
+
+            if (player.velocity.y < 0)
+            {
+                player.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            }
+            else if (player.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                player.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            }
+
+        }
     }
 }
