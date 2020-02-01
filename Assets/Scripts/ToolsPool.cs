e66500b0b0e37dd5cea46910a232b3788a17b6aa
show50726 <k40526@hotmail.com>
2020-02-01 19:34:47 +0800

    add tools pool

--- Assets/Scripts/ToolsPool.cs
+++ Assets/Scripts/ToolsPool.cs
@@ -6,16 +6,19 @@ public class ToolsPool : MonoBehaviour
 {
     public GameObject toolOffer;
     public Player.characterType toolType;
+    public float y0;
+    public float amplitude;
+    public float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        y0 = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(transform.position.x,y0+amplitude*Mathf.Sin(speed*Time.time),transform.position.z);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
