fb60cccbe7c6c5146bafd68dcc21a97359d2fce2
show50726 <k40526@hotmail.com>
2020-02-01 14:12:34 +0800

    add dotween and some state scripts

--- Assets/Scripts/MainTree.cs
+++ Assets/Scripts/MainTree.cs
@@ -5,12 +5,53 @@ using UnityEngine;
 public class MainTree : MonoBehaviour
 {
     public GameObject[] Leaves;
-    public float HP;
+    public GameObject[] Wounds;
+    public float curHP;
+    public float maxHP;
+
+    public bool isDead = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Reset();
+    }
+
+    public void Reset(){
+        curHP = maxHP;
+        isDead = false;
+        foreach(var wound in Wounds){
+            wound.SetActive(false);
+        }
+    }
+
+    public void changeHP(float delta){
+        curHP -= delta;
+        if(curHP < 0){
+            curHP = 0;
+        }
+        else if(curHP > maxHP) {
+            curHP = maxHP;
+        }
+    }
+
+    public void damageTree(GameObject wound, float damage){
+        wound.SetActive(true);
+        //damage effect
+        changeHP(damage);
+    }
+
+    public void healTree(GameObject wound, float healVal){
+        wound.SetActive(false);
+        //heal effect
+        changeHP(healVal);
+    }
+
+    public void Death(){
+        if(!isDead){
+            isDead = true;
+            //dead
+        }
     }
 
     // Update is called once per frame
