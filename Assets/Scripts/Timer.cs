23043b1f3982d90aaf2d408d182069608f4967d5
show50726 <k40526@hotmail.com>
2020-02-02 15:09:46 +0800

    add counter

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 using UnityEngine.UI; //使用Unity UI程式庫。
 
 public class Timer : MonoBehaviour {
-    int time_int=3;
+    int time_int=50;
     public Text time_UI;
+
+    private StateController stateController;
+
+    private bool isStart = false;
     // Start is called before the first frame update
+
+    void Awake() {
+        stateController = GameObject.FindGameObjectWithTag("StateController").GetComponent<StateController>();
+    }
     void Start() {
-        InvokeRepeating("timer",1 ,1);
+        
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if(stateController.state.state == GameManager.State.Running && !isStart){
+            isStart = true;
+            InvokeRepeating("timer",1 ,1);
+        }
     }
 
     // Update is called once per frame
