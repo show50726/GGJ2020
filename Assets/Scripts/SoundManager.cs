3b7e961fd55da98f3b08a720e09570ab64600fd1
Kumama <499k0095@gmail.com>
2020-02-02 15:37:00 +0800

    Add Sound Manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource audiosource;
+
+    public AudioClip music;
+    public AudioClip JumpSFX;
+    public AudioClip UseItem;
+    public AudioClip GetItem;
+
+    public GameObject MusicBk;
+    public static bool IsHaveMusicBk = false;
+    private GameObject clone;
+
+    void Start()
+    {
+        if (!IsHaveMusicBk)
+        {
+            clone = Instantiate(MusicBk, transform.position, transform.rotation) as GameObject;
+            IsHaveMusicBk = true;
+            DontDestroyOnLoad(clone);
+        }
+
+        audiosource = GetComponent<AudioSource>();
+        audiosource.clip = music;
+        audiosource.Play();
+    }
+
+    void Update()
+    {
+
+    }
+}
